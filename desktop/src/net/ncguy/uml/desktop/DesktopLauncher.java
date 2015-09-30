bccfbbaeda133951dd54f5d6b6259dc9a05295c5
ncguy2 <nick.guy@hotmail.co.uk>
2015-09-30 20:09:50 +0100

    Initial Commit

---
+++ desktop/src/net/ncguy/uml/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.ncguy.uml.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.ncguy.uml.UMLLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UMLLauncher(), config);
+	}
+}
